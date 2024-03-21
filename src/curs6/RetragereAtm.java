276a2fcaaf30d25be6763d5a8505b81719a81c95
Andreea Dutia <andreea.balu@gmail.com>
2024-03-21 10:29:07 +0200

    curs6 intermediar

---
+++ src/curs6/RetragereAtm.java
@@ -0,0 +1,39 @@
+package curs6;
+
+import java.util.Scanner;
+
+/*
+ * simulam o retragee de la un bancomat
+ * avem un sold initial (1500)
+ * intreb userul cat vrea sa retraga si verific daca suma nu este mai mare decat soldul
+ * daca suma e mai mare, ii printez fonduri insuficiente si il rog sa incerce din nou
+ * la final printez retragere cu succes si noul sold
+ * 
+ */
+
+public class RetragereAtm {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int sold = 1500;
+		System.out.println("Ce suma vrei sa retragi?");
+		int suma = scan.nextInt();
+		
+		while (suma>sold || suma <= 0) {
+			
+			if (suma>sold) {
+				System.out.println("Fonduri insuficiente!");
+			}else {
+				System.out.println("Suma invalida! Introdu o suma mai mare ca 0.");
+			}
+			
+			System.out.println("Ce suma vrei sa retragi?");
+			 suma = scan.nextInt();;
+		}
+		sold = sold - suma;
+		System.out.println("Retragere cu success! Noul sold este: " + sold);
+		
+
+	}
+
+}
